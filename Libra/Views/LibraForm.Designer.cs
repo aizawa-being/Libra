b6c02cda73556c852fcc4ead1a9305208628de96
aizawa-being <aizawa@beingcorp.co.jp>
2023-06-23 17:25:05 +0900

    【書籍一覧画面】DataGridViewの行番号を記述ロジックの追加。

--- Libra/Views/LibraForm.Designer.cs
+++ Libra/Views/LibraForm.Designer.cs
@@ -26,101 +26,105 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LibraForm));
             this.rentalPanel = new System.Windows.Forms.Panel();
-            this.UserNameLabel = new System.Windows.Forms.Label();
-            this.UserNameTextBox = new System.Windows.Forms.TextBox();
-            this.ReturnButton = new System.Windows.Forms.Button();
-            this.BorrowButton = new System.Windows.Forms.Button();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.returnButton = new System.Windows.Forms.Button();
+            this.borrowButton = new System.Windows.Forms.Button();
             this.searchPanel = new System.Windows.Forms.Panel();
-            this.ClearButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
             this.searchButton = new System.Windows.Forms.Button();
             this.searchWordTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.searchWordLabel = new System.Windows.Forms.Label();
             this.booksDataGridView = new System.Windows.Forms.DataGridView();
-            this.BooksContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.貸出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.返却ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.検索条件のクリアToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.書籍追加ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.書籍削除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.DeleteBookButton = new System.Windows.Forms.Button();
-            this.AddBookButton = new System.Windows.Forms.Button();
-            this.booksDataSet = new Libra.Models.BooksDataSet();
-            this.booksBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.bookIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.userNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.authorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.publisherDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BooksContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.貸出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.返却ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.検索条件のクリアToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.書籍追加ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.書籍削除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.booksBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.booksDataSet = new Libra.Models.BooksDataSet();
+            this.deleteBookButton = new System.Windows.Forms.Button();
+            this.addBookButton = new System.Windows.Forms.Button();
             this.rentalPanel.SuspendLayout();
             this.searchPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.booksDataGridView)).BeginInit();
             this.BooksContextMenuStrip.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // rentalPanel
             // 
-            this.rentalPanel.Controls.Add(this.UserNameLabel);
-            this.rentalPanel.Controls.Add(this.UserNameTextBox);
-            this.rentalPanel.Controls.Add(this.ReturnButton);
-            this.rentalPanel.Controls.Add(this.BorrowButton);
+            this.rentalPanel.Controls.Add(this.userNameLabel);
+            this.rentalPanel.Controls.Add(this.userNameTextBox);
+            this.rentalPanel.Controls.Add(this.returnButton);
+            this.rentalPanel.Controls.Add(this.borrowButton);
             resources.ApplyResources(this.rentalPanel, "rentalPanel");
             this.rentalPanel.Name = "rentalPanel";
             // 
-            // UserNameLabel
+            // userNameLabel
             // 
-            resources.ApplyResources(this.UserNameLabel, "UserNameLabel");
-            this.UserNameLabel.Name = "UserNameLabel";
+            resources.ApplyResources(this.userNameLabel, "userNameLabel");
+            this.userNameLabel.Name = "userNameLabel";
             // 
-            // UserNameTextBox
+            // userNameTextBox
             // 
-            resources.ApplyResources(this.UserNameTextBox, "UserNameTextBox");
-            this.UserNameTextBox.Name = "UserNameTextBox";
+            resources.ApplyResources(this.userNameTextBox, "userNameTextBox");
+            this.userNameTextBox.Name = "userNameTextBox";
             // 
-            // ReturnButton
+            // returnButton
             // 
-            resources.ApplyResources(this.ReturnButton, "ReturnButton");
-            this.ReturnButton.Name = "ReturnButton";
-            this.ReturnButton.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.returnButton, "returnButton");
+            this.returnButton.Name = "returnButton";
+            this.returnButton.UseVisualStyleBackColor = true;
+            this.returnButton.Click += new System.EventHandler(this.ReturnButtonClick);
             // 
-            // BorrowButton
+            // borrowButton
             // 
-            resources.ApplyResources(this.BorrowButton, "BorrowButton");
-            this.BorrowButton.Name = "BorrowButton";
-            this.BorrowButton.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.borrowButton, "borrowButton");
+            this.borrowButton.Name = "borrowButton";
+            this.borrowButton.UseVisualStyleBackColor = true;
+            this.borrowButton.Click += new System.EventHandler(this.BorrowButtonClick);
             // 
             // searchPanel
             // 
             resources.ApplyResources(this.searchPanel, "searchPanel");
-            this.searchPanel.Controls.Add(this.ClearButton);
+            this.searchPanel.Controls.Add(this.clearButton);
             this.searchPanel.Controls.Add(this.searchButton);
             this.searchPanel.Controls.Add(this.searchWordTextBox);
-            this.searchPanel.Controls.Add(this.label1);
+            this.searchPanel.Controls.Add(this.searchWordLabel);
             this.searchPanel.Name = "searchPanel";
             // 
-            // ClearButton
+            // clearButton
             // 
-            resources.ApplyResources(this.ClearButton, "ClearButton");
-            this.ClearButton.Name = "ClearButton";
-            this.ClearButton.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.clearButton, "clearButton");
+            this.clearButton.Name = "clearButton";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButtonClick);
             // 
             // searchButton
             // 
             resources.ApplyResources(this.searchButton, "searchButton");
             this.searchButton.Name = "searchButton";
             this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.SearchButtonClick);
             // 
             // searchWordTextBox
             // 
             resources.ApplyResources(this.searchWordTextBox, "searchWordTextBox");
             this.searchWordTextBox.Name = "searchWordTextBox";
             // 
-            // label1
+            // searchWordLabel
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
+            resources.ApplyResources(this.searchWordLabel, "searchWordLabel");
+            this.searchWordLabel.Name = "searchWordLabel";
             // 
             // booksDataGridView
             // 
@@ -145,64 +149,7 @@
             this.booksDataGridView.ReadOnly = true;
             this.booksDataGridView.RowTemplate.Height = 21;
             this.booksDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            // 
-            // BooksContextMenuStrip
-            // 
-            this.BooksContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.貸出ToolStripMenuItem,
-            this.返却ToolStripMenuItem,
-            this.検索条件のクリアToolStripMenuItem,
-            this.書籍追加ToolStripMenuItem,
-            this.書籍削除ToolStripMenuItem});
-            this.BooksContextMenuStrip.Name = "BooksContextMenuStrip";
-            resources.ApplyResources(this.BooksContextMenuStrip, "BooksContextMenuStrip");
-            // 
-            // 貸出ToolStripMenuItem
-            // 
-            this.貸出ToolStripMenuItem.Name = "貸出ToolStripMenuItem";
-            resources.ApplyResources(this.貸出ToolStripMenuItem, "貸出ToolStripMenuItem");
-            // 
-            // 返却ToolStripMenuItem
-            // 
-            this.返却ToolStripMenuItem.Name = "返却ToolStripMenuItem";
-            resources.ApplyResources(this.返却ToolStripMenuItem, "返却ToolStripMenuItem");
-            // 
-            // 検索条件のクリアToolStripMenuItem
-            // 
-            this.検索条件のクリアToolStripMenuItem.Name = "検索条件のクリアToolStripMenuItem";
-            resources.ApplyResources(this.検索条件のクリアToolStripMenuItem, "検索条件のクリアToolStripMenuItem");
-            // 
-            // 書籍追加ToolStripMenuItem
-            // 
-            this.書籍追加ToolStripMenuItem.Name = "書籍追加ToolStripMenuItem";
-            resources.ApplyResources(this.書籍追加ToolStripMenuItem, "書籍追加ToolStripMenuItem");
-            // 
-            // 書籍削除ToolStripMenuItem
-            // 
-            this.書籍削除ToolStripMenuItem.Name = "書籍削除ToolStripMenuItem";
-            resources.ApplyResources(this.書籍削除ToolStripMenuItem, "書籍削除ToolStripMenuItem");
-            // 
-            // DeleteBookButton
-            // 
-            resources.ApplyResources(this.DeleteBookButton, "DeleteBookButton");
-            this.DeleteBookButton.Name = "DeleteBookButton";
-            this.DeleteBookButton.UseVisualStyleBackColor = true;
-            // 
-            // AddBookButton
-            // 
-            resources.ApplyResources(this.AddBookButton, "AddBookButton");
-            this.AddBookButton.Name = "AddBookButton";
-            this.AddBookButton.UseVisualStyleBackColor = true;
-            // 
-            // booksDataSet
-            // 
-            this.booksDataSet.DataSetName = "BooksDataSet";
-            this.booksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // booksBindingSource
-            // 
-            this.booksBindingSource.DataMember = "BooksDataTable";
-            this.booksBindingSource.DataSource = this.booksDataSet;
+            this.booksDataGridView.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.BooksGridCellPainting);
             // 
             // bookIdDataGridViewTextBoxColumn
             // 
@@ -246,12 +193,72 @@
             this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
             this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
             // 
+            // BooksContextMenuStrip
+            // 
+            this.BooksContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.貸出ToolStripMenuItem,
+            this.返却ToolStripMenuItem,
+            this.検索条件のクリアToolStripMenuItem,
+            this.書籍追加ToolStripMenuItem,
+            this.書籍削除ToolStripMenuItem});
+            this.BooksContextMenuStrip.Name = "BooksContextMenuStrip";
+            resources.ApplyResources(this.BooksContextMenuStrip, "BooksContextMenuStrip");
+            // 
+            // 貸出ToolStripMenuItem
+            // 
+            this.貸出ToolStripMenuItem.Name = "貸出ToolStripMenuItem";
+            resources.ApplyResources(this.貸出ToolStripMenuItem, "貸出ToolStripMenuItem");
+            // 
+            // 返却ToolStripMenuItem
+            // 
+            this.返却ToolStripMenuItem.Name = "返却ToolStripMenuItem";
+            resources.ApplyResources(this.返却ToolStripMenuItem, "返却ToolStripMenuItem");
+            // 
+            // 検索条件のクリアToolStripMenuItem
+            // 
+            this.検索条件のクリアToolStripMenuItem.Name = "検索条件のクリアToolStripMenuItem";
+            resources.ApplyResources(this.検索条件のクリアToolStripMenuItem, "検索条件のクリアToolStripMenuItem");
+            // 
+            // 書籍追加ToolStripMenuItem
+            // 
+            this.書籍追加ToolStripMenuItem.Name = "書籍追加ToolStripMenuItem";
+            resources.ApplyResources(this.書籍追加ToolStripMenuItem, "書籍追加ToolStripMenuItem");
+            // 
+            // 書籍削除ToolStripMenuItem
+            // 
+            this.書籍削除ToolStripMenuItem.Name = "書籍削除ToolStripMenuItem";
+            resources.ApplyResources(this.書籍削除ToolStripMenuItem, "書籍削除ToolStripMenuItem");
+            // 
+            // booksBindingSource
+            // 
+            this.booksBindingSource.DataMember = "BooksDataTable";
+            this.booksBindingSource.DataSource = this.booksDataSet;
+            // 
+            // booksDataSet
+            // 
+            this.booksDataSet.DataSetName = "BooksDataSet";
+            this.booksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // deleteBookButton
+            // 
+            resources.ApplyResources(this.deleteBookButton, "deleteBookButton");
+            this.deleteBookButton.Name = "deleteBookButton";
+            this.deleteBookButton.UseVisualStyleBackColor = true;
+            this.deleteBookButton.Click += new System.EventHandler(this.DeleteBookButtonClick);
+            // 
+            // addBookButton
+            // 
+            resources.ApplyResources(this.addBookButton, "addBookButton");
+            this.addBookButton.Name = "addBookButton";
+            this.addBookButton.UseVisualStyleBackColor = true;
+            this.addBookButton.Click += new System.EventHandler(this.AddBookButtonClick);
+            // 
             // LibraForm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.AddBookButton);
-            this.Controls.Add(this.DeleteBookButton);
+            this.Controls.Add(this.addBookButton);
+            this.Controls.Add(this.deleteBookButton);
             this.Controls.Add(this.booksDataGridView);
             this.Controls.Add(this.rentalPanel);
             this.Controls.Add(this.searchPanel);
@@ -262,8 +269,8 @@
             this.searchPanel.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.booksDataGridView)).EndInit();
             this.BooksContextMenuStrip.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -271,18 +278,18 @@
         #endregion
 
         private System.Windows.Forms.Panel rentalPanel;
-        private System.Windows.Forms.Button ReturnButton;
-        private System.Windows.Forms.Button BorrowButton;
+        private System.Windows.Forms.Button returnButton;
+        private System.Windows.Forms.Button borrowButton;
         private System.Windows.Forms.Panel searchPanel;
-        private System.Windows.Forms.Label UserNameLabel;
-        private System.Windows.Forms.TextBox UserNameTextBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label searchWordLabel;
         private System.Windows.Forms.TextBox searchWordTextBox;
-        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.Button searchButton;
         private System.Windows.Forms.DataGridView booksDataGridView;
-        private System.Windows.Forms.Button DeleteBookButton;
-        private System.Windows.Forms.Button AddBookButton;
+        private System.Windows.Forms.Button deleteBookButton;
+        private System.Windows.Forms.Button addBookButton;
         private System.Windows.Forms.ContextMenuStrip BooksContextMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem 貸出ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 返却ToolStripMenuItem;
--- Libra/Views/LibraForm.cs
+++ Libra/Views/LibraForm.cs
@@ -13,5 +13,48 @@ namespace Libra {
         public LibraForm() {
             InitializeComponent();
         }
+
+        private void BooksGridCellPainting(object sender, DataGridViewCellPaintingEventArgs e) {
+            if (e.ColumnIndex < 0 && 0 <= e.RowIndex) {
+                // セルを描画
+                e.Paint(e.ClipBounds, DataGridViewPaintParts.All);
+
+                // 行番号を描画
+                TextRenderer.DrawText(
+                    e.Graphics,
+                    (e.RowIndex + 1).ToString(),
+                    e.CellStyle.Font,
+                    e.CellBounds,
+                    e.CellStyle.ForeColor,
+                    TextFormatFlags.Right | TextFormatFlags.VerticalCenter);
+
+                // 描画完了
+                e.Handled = true;
+            }
+        }
+
+        private void BorrowButtonClick(object sender, EventArgs e) {
+
+        }
+
+        private void ReturnButtonClick(object sender, EventArgs e) {
+
+        }
+
+        private void SearchButtonClick(object sender, EventArgs e) {
+
+        }
+
+        private void ClearButtonClick(object sender, EventArgs e) {
+
+        }
+
+        private void AddBookButtonClick(object sender, EventArgs e) {
+
+        }
+
+        private void DeleteBookButtonClick(object sender, EventArgs e) {
+
+        }
     }
 }
